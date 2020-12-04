feb63a9fb16823919263c5cee98e355afc7bc95f
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2020-12-03 22:57:12 -0800

    day 4 complete!
    wow ok maybe I should learn regex. Putting that on my to-do list...

---
+++ scratch.js
@@ -0,0 +1,21 @@
+dic = {}
+console.log(!dic["dic"]);
+
+
+
+
+
+
+// hcl:f1aa8a
+
+// hcl:#fffffd 
+
+
+let regex = /^[0-9a-zA-Z]+$/
+
+console.log(regex);
+
+console.log(/^[0-9a-zA-Z]+$/.test("f1aaa"))
+
+let result = 8144 / (8171)
+console.log(result);
